43c028352d397f37bb4634e7d3b5f5c2152b9068
Danny <danny.koshik@gmail.com>
2025-11-01 00:45:11 +0300

    Fix: добавлены пустые строки между методами в FilmStorage и UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,8 +4,12 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film add(Film film);
+
     Film update(Film film);
+
     Film getById(int id);
+
     List<Film> getAll();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -4,8 +4,12 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User add(User user);
+
     User update(User user);
+
     User getById(int id);
+    
     List<User> getAll();
 }
