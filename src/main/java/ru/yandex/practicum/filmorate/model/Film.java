b067f6a04c6b516266fc582fb9ed09da7d3985c7
Danny <danny.koshik@gmail.com>
2025-11-24 21:55:34 +0300

    Use FriendshipStatus map in User; adjust UserService logic for friend requests and confirmations and add DB diagram, DDL and SQL examples

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,4 +11,25 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private int duration;
+<<<<<<< HEAD
+=======
+
+    private Set<Integer> genreIds = new HashSet<>();
+
+    private Integer mpaRatingId;
+
+    private final Set<Integer> likes = new HashSet<>();
+
+    public void addLike(int userId) {
+        likes.add(userId);
+    }
+
+    public void removeLike(int userId) {
+        likes.remove(userId);
+    }
+
+    public int getLikes() {
+        return likes.size();
+    }
+>>>>>>> 6d84347 (Use FriendshipStatus map in User; adjust UserService logic for friend requests and confirmations and add DB diagram, DDL and SQL examples)
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,11 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+<<<<<<< HEAD
+=======
+import java.util.HashMap;
+import java.util.Map;
+>>>>>>> 6d84347 (Use FriendshipStatus map in User; adjust UserService logic for friend requests and confirmations and add DB diagram, DDL and SQL examples)
 
 @Data
 public class User {
@@ -11,4 +16,8 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+<<<<<<< HEAD
+=======
+    private final Map<Integer, FriendshipStatus> friends = new HashMap<>();
+>>>>>>> 6d84347 (Use FriendshipStatus map in User; adjust UserService logic for friend requests and confirmations and add DB diagram, DDL and SQL examples)
 }
