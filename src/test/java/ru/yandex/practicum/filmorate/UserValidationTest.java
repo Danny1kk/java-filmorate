0c3a6493234d62839cf274074b48b3bdcb737cd9
Danny <danny.koshik@gmail.com>
2026-01-12 02:00:07 +0300

    Fix UserValidationTest and RatingDbStorageTest â№3

--- src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
@@ -62,11 +62,11 @@ public class UserValidationTest {
     void shouldThrowExceptionIfEmailIsWrong() {
         user.setEmail(null);
         ValidationException e = assertThrows(ValidationException.class, () -> addUser(user));
-        assertTrue(e.getMessage().contains("Email не может быть пустой и должен содержать символ '@'"));
+        assertTrue(e.getMessage().contains("Email не может быть пустым и должен содержать символ '@'"));
 
         user.setEmail("wrongemail.ru");
         e = assertThrows(ValidationException.class, () -> addUser(user));
-        assertTrue(e.getMessage().contains("Email не может быть пустой и должен содержать символ '@'"));
+        assertTrue(e.getMessage().contains("Email не может быть пустым и должен содержать символ '@'"));
     }
 
     @Test
